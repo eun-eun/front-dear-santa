35065af77ad5bbddf22dc3b41cd55e6e51a61439
easyhe <euneundh@gmail.com>
2024-06-01 22:10:16 +0900

    환경설정

--- next.config.js
+++ next.config.js
@@ -5,12 +5,6 @@ const nextConfig = {
   images: {
     loaderFile: './image-loader.js',
   },
-  generateBuildId: async () => {
-    const timestamp = new Date().getTime().toString();
-    const randomStr = Math.random().toString(36).substring(2);
-    const buildId = `${process.env.NEXT_PUBLIC_VERSION}-${timestamp}-${randomStr}`;
-    return buildId;
-  },
   experimental: {
     appDir: true,
   },
